f4e0179c07cae7f0d30f2dc6069618155fba459b
FusionAge <websites@fusionage.com>
2023-01-04 14:40:43 +0000

    Get rid of Poetry

--- app.py
+++ app.py
@@ -341,4 +341,4 @@ def get_stats(myid):
 
 #MAIN EXECUTION
 if __name__ == '__main__':
-  app.run(ssl_context="adhoc",debug=True, host="0.0.0.0", port=80)
+  app.run(debug=True, host="0.0.0.0", port=80)
